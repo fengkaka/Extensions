d25213ea66383cd0dce159c31b00a52b27c75139
Hao Kung <HaoK@users.noreply.github.com>
2018-12-11 14:46:13 -0800

    Add using

--- src/Shared/test/CertificateManagerTests.cs
+++ src/Shared/test/CertificateManagerTests.cs
@@ -10,6 +10,7 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Microsoft.AspNetCore.Testing.xunit;
 using Xunit;
 using Xunit.Abstractions;
 
